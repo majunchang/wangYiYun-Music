54d1253a78776b7e19d8eb1a726dfab35ba91b20
majunchang <2471978285@qq.com>
2017-08-30 14:21:10 +0800

    提取mixin的公共代码

--- src/common/js/cache.js
+++ src/common/js/cache.js
@@ -7,6 +7,9 @@ import storage from 'good-storage'
 const search_key = '_search_';
 const ArrMaxLen = 15;
 
+const favorite_key = '_favorite_';
+const favoriteMaxLen = 200;
+
 function insertArr(arr, val, compare, maxLen) {
   var index = arr.findIndex(compare);
   if (index === 0) {
@@ -22,6 +25,7 @@ function insertArr(arr, val, compare, maxLen) {
   }
 }
 
+// 保存搜索历史 以及获取搜索历史
 export function saveSearch(query) {
   let searches = storage.get(search_key, []);
   /* 在这里声明一个方法  用来帮助我们 查找当前query在arr中的位置
@@ -36,6 +40,45 @@ export function saveSearch(query) {
 }
 
 export function loadSearch() {
-  console.log(storage.get(search_key,[]));
-  return storage.get(search_key,[])
+  console.log(storage.get(search_key, []));
+  return storage.get(search_key, [])
 }
+
+export function saveFavorite(songTarget) {
+  let songs = storage.get(favorite_key, []);
+  insertArr(songs, songTarget, (item) => {
+    return item.id === songTarget.id;
+  },favoriteMaxLen);
+  storage.set(favorite_key,songs);
+  return songs;
+}
+
+export function deleteSearch(query) {
+
+  let searches = storage.get(search_key,[]);
+  deleteFromArray(searches,(item)=>{
+    return item === query
+  })
+  storage.set(search_key,searches);
+  return searches;
+}
+function deleteFromArray(arr,compare) {
+  const index = arr.findIndex(compare);
+  if(index>-1){
+    app.splice(index,1);
+  }
+}
+export function deleteFavorite(song) {
+  let songs = storage.get(FAVORITE_KEY, [])
+  deleteFromArray(songs, (item) => {
+    return item.id === song.id
+  })
+  storage.set(FAVORITE_KEY, songs)
+  return songs
+}
+
+export function loadFavorite() {
+  return storage.get(favorite_key,[]);
+}
+
+
